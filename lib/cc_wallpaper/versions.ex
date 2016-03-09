6b7d6ebbbf950ebe5c2c4c7123d55a00e67d17b0
awea <aweaoftheworld@gmail.com>
2016-03-09 14:57:58 +0100

    use dominant color to define watermark

--- lib/cc_wallpaper/versions.ex
+++ lib/cc_wallpaper/versions.ex
@@ -1,18 +1,35 @@
 defmodule Mix.Tasks.CcWallpaper do
   use Mix.Task
 
-  defmodule DominantColor do
+  defmodule DominantColors do
     @shortdoc "Output dominant image color"
 
-    def run(_) do
-      # width: 1022 height: 1392
-      output   = :os.cmd('convert #{System.cwd()}/datas/portrait.jpg -format %c -depth 8  histogram:info: | sort -n | tail -1') |> to_string 
-      # => '686750: (228,227,222) #E4E3DE srgb(228,227,222)'
-      hexColor = Regex.run(~r/#([0-9A-Z]*)/, output) |> List.last
-      # => "E4E3DE"
+    def run(args) do
+      {opts, _, _} = OptionParser.parse(args, aliases: [p: :path], strict: [path: :string]) 
+      image_path   = opts[:path]
+
+      IO.inspect get(image_path)
+    end
+
+    def get(image_path) do
+      output   = :os.cmd('convert #{image_path} -format %c -depth 8  histogram:info: | sort -n | tail -2') |> to_string 
+      |> String.split( "\n")
+      
+      colors = Enum.filter(output, fn(x) -> String.length(x) > 0 end) |> Enum.sort |> Enum.map(fn(x) -> 
+        [int, hex] = Regex.run(~r/(\d*):.*#([0-9A-Z]*)/, x) |> List.delete_at(0)
+        int        = String.to_integer(int)
+        hex        = String.to_integer(hex, 16)
+
+        [int, hex]
+      end)
+      # => [[189642, 16749042], [1208907, 5243125]]
+      # [[num, color], [num, color]] last is dominant color
+      [secondColor | [dominantColor]] = colors
+      [_, dominantColor]              = dominantColor
+      [_, secondColor]                = secondColor
       # list to integer: https://github.com/rjsamson/hexate/blob/master/lib/hexate.ex
 
-      IO.inspect(hexColor)
+      {dominantColor, secondColor}  
     end
   end
 
@@ -44,6 +61,7 @@ defmodule Mix.Tasks.CcWallpaper do
     @desktop_sizes %{desktop_a: "1280x800", desktop_b: "1440x900", desktop_c: "1680x1050", desktop_d: "1920x1200", desktop_e: "2560x1440", desktop_f: "3840x2400"}
     @iphone_sizes %{iphone_5: "640x1136", iphone_6: "750x1334", iphone_6_plus: "1080x1920"}
     @ipad_sizes %{ipad: "2048x2048"}
+    @color_limit_integer 8388607
 
     def run(args) do
       # http://is.gd/TWGGvC
@@ -61,22 +79,36 @@ defmodule Mix.Tasks.CcWallpaper do
         File.mkdir(output_dirname)
       end
 
+      # choose watermark color based on dominant color
+      if watermark do
+        {dominantColor, _} = DominantColors.get(image_path)
+
+        if dominantColor < @color_limit_integer do
+          watermark_color = "dark"
+        else
+          watermark_color = "light"
+        end
+      end
+
       Enum.filter(sizes, fn{_, size} -> 
         size = String.replace(size, "x", "")
         String.to_integer(size) <= String.to_integer("#{image_infos.width}#{image_infos.height}")
       end)
       |> Enum.each(fn{k, size} -> 
-        new_file_name  = "#{file_name_convention(output_basename, k, size, image_infos.ext)}"
         new_image_path = "#{output_dirname}/#{file_name_convention(output_basename, k, size, image_infos.ext)}"
         
         image |> copy |> resize_to_fill(size) |> save(new_image_path) 
 
         # add a watermark
         if watermark do
-          args = ~w(-gravity SouthWest #{System.cwd()}/datas/watermarks/watermark-height-#{watermark_size(size)}-dark.png #{new_image_path} #{new_image_path})
+          args = ~w(-gravity SouthWest #{System.cwd()}/datas/watermarks/watermark-height-#{watermark_size(size)}-#{watermark_color}.png #{new_image_path} #{new_image_path})
           System.cmd "composite", args, stderr_to_stdout: true
         end
       end)
+    end
+
+    defp watermark_color(dominant_color) do
+      
     end
 
     defp watermark_size(size) do
